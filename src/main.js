0de38ae9bd12fcc1fe9ec45fe4b6ac692ae1d4e3
LeiYuAurora <lantsp05@gmail.com>
2024-05-12 12:19:55 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
-console.log("Start to load");
 import "../modules/drag-drop-touch.js";
-console.log("drag-drop-touch loaded");
 import "./shims.js";
-console.log("shims loaded");
 import "./merge-globals.js";
-console.log("merge-globals loaded");
 import { browserCheck, init } from "./game.js";
 import { DEV } from "./env.js";
 
